1b8f61d8a22bb713322cdc6b2e8d9f9bf77c3b82
John <johnnylamannajr@gmail.com>
2022-12-10 16:35:37 -0800

    making nametag

--- app.js
+++ app.js
@@ -1,3 +1,23 @@
-// grab DOM elements
+// const booger = document.getElementById('nametag');
+// booger.style.backgroundColor = 'purple';
 
-// set event listeners
+const nameDisplay = document.getElementById('name-display');
+const nameInput = document.getElementById('name-input');
+const button = document.getElementById('update-button');
+button.addEventListener('click', () => {
+    console.log('i am clicking the button');
+    console.log(nameDisplay);
+    // nameDisplay.textContent = 'Johnny';
+    nameDisplay.textContent = nameInput.value;
+    nameDisplay.style.color = 'blue';
+});
+
+const pronounDisplay = document.getElementById('pronoun-display');
+const pronounInput = document.getElementById('pronoun-input');
+const otherButton = document.getElementById('update-button');
+otherButton.addEventListener('click', () => {
+    console.log('i am clicking the button');
+    console.log(pronounDisplay);
+    pronounDisplay.textContent = "(" + pronounInput.value + ")";
+
+});
